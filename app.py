b87e59d470d7d3cf6034f33f2e3c3fb73e9d8a64
botul <pawel.botulinski@gmail.com>
2022-03-17 23:24:15 +0100

    feat: blog application added

--- app.py
+++ app.py
@@ -27,5 +27,38 @@ def brudnopis():
     hero = character(name)
     return render_template("brudnopis.html", hero=hero, super_heroes=super_heroes)
 
+@app.route('/heroes')
+def heroes():
+    super_heroes = ['Skiba', 'Queen', 'The Prodigy']
+    content_list = []
+    # for i in range(3):
+    #     interesting_character = choice(super_heroes)
+    #     interesting_character_index = super_heroes.index(interesting_character)
+    #     super_heroes.pop(interesting_character_index)
+
+    #     character_desc = character(super_heroes)
+
+    #     word_list = character_desc.split(' ')
+    #     word_list_len = len(word_list)
+
+    #     content = [interesting_character, character_desc, word_list_len]
+
+    # chosen_hero = random.choice( super_heroes)
+    # hero = character(name)
+    # return render_template("brudnopis.html", hero=hero, super_heroes=super_heroes)
+
+posts = [
+    {
+        'author': 'Botul',
+        'title': 'Wpis w blogu',
+        'content': 'Taki sobie wpis w blogu',
+        'date_posted': '17 marca 2022'
+    }
+        ]
+@app.route('/blog')
+def blog():
+    return render_template("blog.html", posts=posts)
+
+
 if __name__=="__main__":
     app.run()
