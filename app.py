200cbb7c962805945f72bbb1edd1d8fac82f1a4a
botul <pawel.botulinski@gmail.com>
2022-03-21 22:37:39 +0100

    fix: fixed heroes function

--- app.py
+++ app.py
@@ -9,42 +9,23 @@ def index():
     text = open('dane/xd.txt').read()
     return render_template("index.html", text=text)
 
-@app.route('/xd')
-def xd():
-    return render_template("xd.html")
-
-@app.route('/ua')
-def ua():
-    text = "Dużo siły i wytrwałości dla Ukrainy"
-    text2 = "Jesteśmy z Wami!"
-    return render_template("ua.html", text=text, text2=text2)
-
-# @app.route('/brudnopis')
-# def brudnopis():
-#     name = "Kłapouchy"
-#     super_heroes = ['Sowa', 'Tygrysek', 'Kubuś']
-#     chosen_hero = random.choice( super_heroes)
-#     hero = character(name)
-#     return render_template("brudnopis.html", hero=hero, super_heroes=super_heroes)
 
 @app.route('/heroes')
 def heroes():
-    super_heroes = ['Skiba', 'Queen', 'The Prodigy']
+    super_heroes = ['Krzysztof Skiba', 'Queen', 'The Prodigy']
     for i in range(3):
         interesting_character = random.choice(super_heroes)
-        interesting_character_index = super_heroes.index(interesting_character)
-        super_heroes.pop(interesting_character_index)
+        character_index = super_heroes.index(interesting_character)
+        super_heroes.pop(character_index)
 
-        character_desc = character(super_heroes)
+        character_desc = character(interesting_character)
 
         word_list = character_desc.split(' ')
         word_list_len = len(word_list)
 
         content = [interesting_character, character_desc, word_list_len]
 
-    chosen_hero = random.choice( super_heroes)
-    hero = character(name)
-    return render_template("hero.html", hero=hero, super_heroes=super_heroes)
+    return render_template("hero.html", super_heroes=super_heroes)
 
 posts = [
     {
