4d7969763b101dd016c53740496cfe38954a76e3
botul <pawel.botulinski@gmail.com>
2022-03-21 21:28:53 +0100

    fix: fixed import from Wikipedia

--- my_apps/wiki_character.py
+++ my_apps/wiki_character.py
@@ -6,10 +6,10 @@ wiki.set_lang("pl")
 def character( name):
 
     page = wiki.search( name)
-    #character_page = wiki.page(page[0])
-    #content = character_page.content
+    character_page = wiki.page(page[0])
+    content = character_page.content
     content = wiki.summary(name, sentences=6)
     return content
     
-name = "Bruce Lee"
-print(character(name))
+#name = "Bruce Lee"
+#print(character(name))
